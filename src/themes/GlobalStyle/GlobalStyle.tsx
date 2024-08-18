9189b34d7c19ddd94dd24fc0c29e0395be8867e9
Jessica Quirino <qrnojess@gmail.com>
2024-08-18 16:58:36 -0300

    refactor: implement font in global styles

--- src/themes/GlobalStyle/GlobalStyle.tsx
+++ src/themes/GlobalStyle/GlobalStyle.tsx
@@ -1,14 +1,22 @@
-import { createGlobalStyle } from "styled-components";
-import { IThemeMain } from "../ThemeMain/types";
+import { createGlobalStyle } from 'styled-components';
+import { IThemeMain } from '../ThemeMain/types';
 
 const GlobalStyle = createGlobalStyle<{ theme: IThemeMain }>`
-  body {
-    font-family: ${props => props.theme.typography.primary};
-    background: ${props => props.theme.colors.white}; 
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
+
+  *, *::before, *::after {
+    box-sizing: border-box;
   }
+
+  body {
+    font-family: 'Inter', sans-serif;
+    background: ${(props) => props.theme.colors.white};
+    margin: 0px;
+  }
+
   ::selection {
-    background: ${props => props.theme.colors.primary};
-    color: ${props => props.theme.colors.white};
+    background: ${(props) => props.theme.colors.primary};
+    color: ${(props) => props.theme.colors.white};
   }   
 `;
 
