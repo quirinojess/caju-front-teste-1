9be701bd42066e5cadd3c4586195f66c8aed3919
Jessica Quirino <qrnojess@gmail.com>
2024-08-18 16:27:15 -0300

    refactor: update routes and translate for a user-br experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import { ThemeProvider } from "styled-components";
-import Router from "~/router";
-import { Header } from "~/components";
-import { GlobalStyle, ThemeMain } from "~/themes";
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Header } from '~/components';
+import { GlobalStyle, ThemeMain } from '~/themes';
+import Router from './router';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
- return (
-  <ThemeProvider theme={ThemeMain}>
-   <GlobalStyle />
-   <Header />
-   <Router />
-  </ThemeProvider>
- );
+  return (
+    <ThemeProvider theme={ThemeMain}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Header />
+        <ToastContainer />
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
