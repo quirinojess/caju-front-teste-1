e505dba6cae23bb333d5c345b8a8ffab78e62b26
Jessica Quirino <qrnojess@gmail.com>
2024-08-21 14:33:48 -0300

    fix: warnings and erros in console.log

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,7 +3,7 @@ import * as S from './styled';
 
 const Button = ({ children, onClick, type = 'button', variant }: TButton) => {
   return (
-    <S.Button onClick={onClick} type={type} variant={variant}>
+    <S.Button onClick={onClick} type={type} $variant={variant}>
       {children}
     </S.Button>
   );
--- src/components/Button/styled.ts
+++ src/components/Button/styled.ts
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 import { RowAround } from '~/themes/CommonAligns';
 import { TStatusRegistration } from '~/types/TStatus';
 
-interface StyledButtonProps {
-  variant?: TStatusRegistration;
+type StyledButtonProps = {
+  $variant?: TStatusRegistration;
 }
 
-const getvariantColor = (variant: StyledButtonProps['variant'], theme: any) => {
-  switch (variant) {
+const getVariantColor = ($variant: StyledButtonProps['$variant'], theme: any) => {
+  switch ($variant) {
     case 'REVIEW':
       return theme.colors.secondary;
     case 'REPROVED':
@@ -20,20 +20,21 @@ const getvariantColor = (variant: StyledButtonProps['variant'], theme: any) => {
 };
 
 export const Button = styled(RowAround('button'))<StyledButtonProps>`
-  background-color: ${({ variant, theme }) => getvariantColor(variant, theme)};
-  color: ${({ variant, theme }) => (variant ? theme.colors.white : theme.colors.darkDeep)};
-  box-shadow: ${(props) => props.theme.shadows.dark};
-  border-radius: ${(props) => props.theme.borderRadius.small};
-  font-size: ${({ variant, theme }) => (variant ? theme.typography.p : theme.typography.medium)};
-  padding: ${({ variant, theme }) => (variant ? theme.spacing.small : theme.spacing.medium)} 
-    ${({ variant, theme }) => (variant ? theme.spacing.small : theme.spacing.large)};
-  margin: ${(props) => props.theme.spacing.large} 0;
+  background-color: ${({ $variant, theme }) => getVariantColor($variant, theme)};
+  color: ${({ $variant, theme }) => ($variant ? theme.colors.white : theme.colors.darkDeep)};
+  box-shadow: ${({ theme }) => theme.shadows.dark};
+  border-radius: ${({ theme }) => theme.borderRadius.small};
+  font-size: ${({ $variant, theme }) => ($variant ? theme.typography.p : theme.typography.medium)};
+  padding: ${({ $variant, theme }) => ($variant ? theme.spacing.small : theme.spacing.medium)} 
+    ${({ $variant, theme }) => ($variant ? theme.spacing.small : theme.spacing.large)};
+  margin: ${({ theme }) => theme.spacing.large} 0;
   font-weight: 600;
   outline: none;
   border: none;
   cursor: pointer;
-    @media (max-width: ${({ theme }) => theme.breakpoints.desktop}) {
-  font-size: ${({ variant, theme }) => (variant ? theme.typography.small : theme.typography.p)};
-    padding: ${({ variant, theme }) => (variant ? theme.spacing.verySmall : theme.spacing.small)} 
+  
+  @media (max-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    font-size: ${({ $variant, theme }) => ($variant ? theme.typography.small : theme.typography.p)};
+    padding: ${({ $variant, theme }) => ($variant ? theme.spacing.verySmall : theme.spacing.small)};
   }
 `;
